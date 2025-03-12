6298821f7d3406f92ca7ba454a3be79078ae96c1
sapana <sapanadhami1111@gmail.com>
2025-03-12 14:06:19 +0000

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,43 @@
 import sys 
+import os
 
-def error_message_details(error, error_detail:sys):
-    _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)   
-    )
-    return error_message
+import numpy as np
+import pandas as pd
+
+
+import dill
+from src.exception import CustomException
+
+from sklearn.metrics import r2_score
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
+
+
+def evaluate_models(X_train, y_train, X_test,y_test, models ):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+        return report
     
-class CustomException(Exception):
-    def __init__(self, error_message, error_detail:sys):
-        super().__init__(error_message)
-        self.error_message = error_message_details(error_message, error_detail=error_detail)
-
-    def __str__(self):
-        return self.error_message
-
-
-
+    except Exception as e:
+        raise CustomException(e, sys)
